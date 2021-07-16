4b697cb894e2c637fe54876d9c88452b70fa1dfb
Kevin Dyer <kevin.m.dyer@gmail.com>
2021-07-15 21:21:08 -0600

    Adding basic of the deployer

--- packages/deployer/src/cli.js
+++ packages/deployer/src/cli.js
@@ -1,15 +1,31 @@
-'use strict';
+const fs = require('fs/promises');
+const path = require('path');
 
 const factory = require('yargs/yargs');
-const deployer = require('./deployer');
 
-module.exports = cli;
+const { deployProject } = require('./deployer');
 
-function cli(cwd) {
+const parseArgumentsIntoOptions = async ({ directory }) => {
+  try {
+    const gitIgnoreContents = await fs.readFile(path.join(directory, '.gitignore'));
+    console.log(gitIgnoreContents.toString());
+    const 
+  } catch (err) {
+    console.log(`Failed to read .gitignore`, err);
+  }
+  return {
+    directory: directory,
+  };
+};
+
+exports.cli = (cwd) => {
   const parser = factory(null, cwd);
 
   parser.alias('h', 'help');
   parser.alias('v', 'version');
+
+  parser.alias('d', 'directory')
+  parser.default('directory', process.cwd());
 
   parser.usage(
     "$0",
@@ -19,8 +35,11 @@ function cli(cwd) {
         // TODO: options
       });
     },
-    argv => deployer(argv)
+    async (argv) => {
+      const options = await parseArgumentsIntoOptions(argv);
+      await deployProject(options);
+    }
   );
 
   return parser;
-}
+};
--- packages/deployer/src/deployer.js
+++ packages/deployer/src/deployer.js
@@ -1,7 +1,21 @@
-'use strict';
+const fs = require('fs');
+const path = require('path');
 
-module.exports = deployer;
+const tar = require('tar');
 
-function deployer() {
-    // TODO
-}
+const bundleSourceCode = async (options) => {
+  const response = {
+    output: path.join(options.directory, `source.tar.gz`),
+  };
+  await new Promise((resolve, reject) => {
+    tar.c({ z: true }, [options.directory])
+      .pipe(fs.createWriteStream(response.output))
+      .on('error', reject)
+      .on('finish', resolve);
+  });
+};
+
+exports.deployProject = async (options) => {
+  console.log(options);
+  await bundleSourceCode(options);
+};
